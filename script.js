f533fc6eb96893988d3c05f69c5c5e14b767442d
seyeonsim <sysim@ucalgary.ca>
2022-09-25 21:44:50 -0600

    fixed bug

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const smallCups = document.querySelectorAll('.cup-small')
 const liters= document.getElementById('liters')
 const percentage= document.getElementById('percentage')
 const remained = document.getElementById('remained')
+const wellDone = document.getElementById('wellDone')
 
 updateBigCup()
 var sound = new Audio("pouring.m4a");
@@ -39,16 +40,20 @@ function updateBigCup(){
     if(fullCups === 0){
         percentage.style.visibility = 'hidden'
         percentage.style.height = 0
+        wellDone.style.visibility = 'hidden'
     }else{
         percentage.style.visibility = 'visible'
         percentage.style.height = `${fullCups/totalCups *330}px`
         percentage.innerText = `${fullCups/ totalCups *100}%`
+        wellDone.style.visibility = 'hidden'
+
     }
 
     if(fullCups === totalCups){
         remained.style.visibility='hidden'
         remained.style.height = 0
-        window.alert("Well Done!")
+        wellDone.style.visibility = 'visible'
+        
     }else{
         remained.style.visibility='visible'
         liters.innerText=`${2-(250* fullCups / 1000)}L`
