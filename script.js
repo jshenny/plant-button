8d045bcf4d5e5967a7fc6dad71377f0c93e651f8
jshenny <jshen019@gmail.com>
2022-09-25 21:18:26 -0600

    Added plant watering functionality and all plant images

--- script.js
+++ script.js
@@ -1,10 +1,44 @@
-$(document).ready(function() {
-    $('.water-button').click(function() {
-      if ($('.plant').attr('src') == "images/plant_sprout.svg") {
-        $('.plant').attr("src", "images/plant_teen.svg"); 
-      }
-      else if ($('.plant').attr('src') == "images/plant_teen.svg") {
-        $('.plant').attr("src", "images/plant_adult.svg"); 
-      }
+let isPlantWatered = false;
+
+$(document).ready(function () {
+    $('.water-button').click(function () {
+        isPlantWatered = true;
+        const plantImage = $('.plant').attr('src');
+        if (plantImage === "images/happy_plant_sprout.svg" || plantImage === "images/neutral_plant_sprout.svg" || plantImage === "images/sad_plant_sprout.svg") {
+            $('.plant').attr("src", "images/happy_plant_teen.svg");
+        } else if (plantImage === "images/happy_plant_teen.svg" || plantImage === "images/neutral_plant_teen.svg" || plantImage === "images/sad_plant_teen.svg") {
+            $('.plant').attr("src", "images/happy_plant_adult.svg");
+        } else {
+            $('.plant').attr("src", "images/happy_plant_adult.svg");
+        }
     })
-  })
+})
+
+
+function checkIfPlantIsWatered() {
+    const plantImage = $('.plant').attr('src');
+    if (isPlantWatered === false) {
+        if (plantImage === "images/happy_plant_sprout.svg") {
+            $('.plant').attr("src", "images/neutral_plant_sprout.svg");
+        } else if (plantImage === "images/happy_plant_teen.svg") {
+            $('.plant').attr("src", "images/neutral_plant_teen.svg");
+        } else if (plantImage === "images/happy_plant_adult.svg") {
+            $('.plant').attr("src", "images/neutral_plant_adult.svg");
+        } else if (plantImage === "images/neutral_plant_sprout.svg") {
+            $('.plant').attr("src", "images/sad_plant_sprout.svg");
+        } else if (plantImage === "images/neutral_plant_teen.svg") {
+            $('.plant').attr("src", "images/sad_plant_teen.svg");
+        } else if (plantImage === "images/neutral_plant_adult.svg") {
+            $('.plant').attr("src", "images/sad_plant_adult.svg");
+        }
+    }
+    isPlantWatered = false;
+}
+
+function resetPlant() {
+    $('.plant').attr("src", "images/happy_plant_sprout.svg");
+    isPlantWatered = false;
+}
+
+setInterval(checkIfPlantIsWatered, 5000);
+setInterval(resetPlant, 60000);
