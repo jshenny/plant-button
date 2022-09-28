08d123dd10df1fe776e10f43a28c9640ccbe8b96
Jane Shen <jshen019@gmail.com>
2022-09-27 21:54:16 -0600

    Fixed watering can

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 let isPlantWatered = false;
 
 $(document).ready(function () {
-    $('.water-button').click(function () {
+    $('.add').click(function () {
         isPlantWatered = true;
-        $(".watering-can").css("display", "block");
+        $(".watering-can").css("visibility", "visible");
         setTimeout(function(){
             const plantImage = $('.plant').attr('src');
             if (plantImage === "images/happy_plant_sprout.svg" || plantImage === "images/neutral_plant_sprout.svg" || plantImage === "images/sad_plant_sprout.svg") {
                 $('.plant').attr("src", "images/happy_plant_teen.svg");
+                console.log("1");
             } else if (plantImage === "images/happy_plant_teen.svg" || plantImage === "images/neutral_plant_teen.svg" || plantImage === "images/sad_plant_teen.svg") {
                 $('.plant').attr("src", "images/happy_plant_adult.svg");
+                console.log("2");
             } else {
                 $('.plant').attr("src", "images/happy_plant_adult.svg");
+                console.log("3");
             }
-            $(".watering-can").css("display", "none");
+            $(".watering-can").css("visibility", "hidden");
         }, 1500); 
     })
 })
@@ -45,4 +48,96 @@ function resetPlant() {
 }
 
 setInterval(checkIfPlantIsWatered, 5000);
-setInterval(resetPlant, 60000);
+setInterval(reset, 600000);
+
+
+//Button selectors
+const addButton = document.querySelector(".add");
+    resetButton = document.querySelector(".reset")
+//Element for update
+const currentCupsEl = document.querySelector('.current-cups'),
+    currentLitersEl = document.querySelector('.current-liters')
+const progress = document.querySelector('.percentage')
+const remainedLiters = document.getElementById('remained-liters')
+const wellDone = document.getElementById('wellDone')
+const remained = document.getElementById('remained')
+
+const MAX_CUPS = 8,
+      MIN_CUPS = 0;
+
+let cups = 0,
+    liters =0,
+    percentage =0;
+    remains=2;
+
+updateLayout();
+addButton.addEventListener("click",addCup)
+resetButton.addEventListener("click",reset)
+
+
+function addCup(){
+    cups++;
+    liters+=250;
+    percentage = (cups/MAX_CUPS) *100;
+    
+    //Update Layout
+    updateLayout();
+   
+    if(cups === MAX_CUPS ){
+        addButton.disabled=true;
+    }else{
+        resetButton.disabled = false;
+    }
+    
+
+}
+
+function reset() {
+    resetCup();
+    resetPlant();
+}
+
+function resetCup(){
+    cups =0;
+    liters =0;
+    percentage = 0;
+      //Update Layout
+      updateLayout();
+     
+      addButton.disabled = false;
+      
+      if(cups === MIN_CUPS){
+        resetButton.disabled = true;
+        }else{
+        addButton.disabled=false;
+      }
+}
+
+
+function updateLayout(){
+  
+    currentCupsEl.textContent = `${cups}/8`;
+    currentLitersEl.textContent = `${liters/1000}l/2l`;
+    progress.innerText = `${percentage}%`;
+    progress.style.height= `${percentage}%`;
+    remainedLiters.innerText=`${2-(250* cups / 1000)}L`
+  
+    if(cups === MIN_CUPS){
+        progress.style.visibility = 'hidden'
+        progress.style.height = 0
+        wellDone.style.visibility = 'hidden'
+
+    }else{
+        progress.style.visibility = 'visible'
+    }
+
+    if(cups === MAX_CUPS){
+        remained.style.visibility = 'hidden'
+        remained.style.height = 0
+        wellDone.style.visibility = 'visible'
+
+    }else{
+        remained.style.visibility = 'visible'
+        wellDone.style.visibility = 'hidden'
+    }
+}
